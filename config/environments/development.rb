c92b3d8882660aba62195d3e39b02b55b1143a61
Gordie Smith <agsmith4@gmail.com>
2015-03-04 23:59:24 +0000

    Set up Devise for User

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,10 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-
+  
+  # Configure for Devise
+  config.action_mailer.default_url_options = { host: '0.0.0.0', port: 3000 }
+  
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
