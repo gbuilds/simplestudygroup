bf44e064293c605c599984a8e198339553659912
Gordie Smith <agsmith4@gmail.com>
2015-03-05 19:59:18 +0000

    Generate events controller and add new, show, create actions

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,12 @@
+class EventsController < ApplicationController
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def show
+  end
+  
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   
   belongs_to :city
+  has_many :events, as: "created_events"
 end
