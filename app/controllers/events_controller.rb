dd24f54a2fec01446758e3ebbb19955f6b95c39d
Gordie Smith <agsmith4@gmail.com>
2015-03-06 00:00:19 +0000

    Make a form for creating events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,20 @@
 class EventsController < ApplicationController
   
   def new
+    @event = current_user.created_events.build
   end
   
   def create
+    @event = current_user.created_events.build(event_params)
   end
   
   def show
     @event = Event.find(params[:id])
+  end
+  
+  private
+  
+  def event_params
   end
   
 end
