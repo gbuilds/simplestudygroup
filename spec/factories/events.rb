cfd1bc44ed048ce6ea7026d3308340a1da62de76
Gordie Smith <agsmith4@gmail.com>
2015-03-12 18:34:11 +0000

    Add tests to event spec

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :event do
-    title { "My Event" }
-    description { "This is the description for my event" }
-    start_time { Time.now + 10.hours }
-    end_time   { Time.now + 12.hours }
+    title "My Event"
+    description "This is the description for my event"
+    start_time Time.now + 10.hours
+    end_time   Time.now + 12.hours
   end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,8 +1,37 @@
 require 'rails_helper'
 
 describe Event do
-  it "is invalid without a title"
-  it "is invalid without a description"
-  it "is invalid without a start datetime"
-  it "is invalid without an end datetime"
+  
+  it "is valid with a title, description, start and end datetime" do
+    event = Event.new(title: "Event title",
+      description: "this is the event",
+      start_time: Time.now + 2.hours,
+      end_time: Time.now + 4.hours)
+    expect(event).to be_valid
+  end
+  
+  it "is invalid without a title" do
+    event = build(:event, title: nil)
+    event.valid?
+    expect(event.errors[:title]).to include("can't be blank")
+  end
+  
+  it "is invalid without a description" do
+    event = build(:event, description: nil)
+    event.valid?
+    expect(event.errors[:description]).to include("can't be blank")
+  end
+  
+  it "is invalid without a start datetime" do
+    event = build(:event, start_time: nil)
+    event.valid?
+    expect(event.errors[:start_time]).to include("can't be blank")
+  end
+  
+  it "is invalid without an end datetime" do
+    event = build(:event, end_time: nil)
+    event.valid?
+    expect(event.errors[:end_time]).to include("can't be blank")
+  end
+  
 end
