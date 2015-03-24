26daa17c008d4b5b169ccf880578738f4415a814
Gordie Smith <agsmith4@gmail.com>
2015-03-24 01:39:36 +0000

    Devise mailer config and add a db:seed for Buffalo

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-cities = City.create([{ name: 'Boston'}, { name: 'Seattle' }, { name: 'Chicago'}])
+cities = City.create([{ name: 'Boston'}, { name: 'Seattle' }, { name: 'Chicago'}, { name: 'Buffalo' }])
