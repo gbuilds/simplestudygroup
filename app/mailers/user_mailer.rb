37c5996abdbd1256631b020082b2c1adf3e99526
Gordie Smith <agsmith4@gmail.com>
2015-03-27 21:58:42 +0000

    Add to devise registration controller to send welcome email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: "notifications@studygroup.io"
+  default from: "notifications@getstudygroup.com"
   
   def welcome_email(user)
     @user = user
